3be23a739ad132a5f39224b858873a1c86ed22a9
Rosa, Genesis <mrgenesis@hotmail.com>
2020-05-31 22:12:26 -0400

    Definido rota POST boleto/one retornando o resultado em json

--- routes/boleto.js
+++ routes/boleto.js
@@ -1,15 +1,61 @@
-const router = require('express').Router();
-const boleto = require('./boleto');
+const router = require('express').Router(),
+      moment = require('moment'),
+      Gerencianet = require('gn-api-sdk-node');
 
-
+router.get('/test', (req, res) => {
+    res.status(200).send(req.body.test);
+});
+router.get('/one', (req, res) => {
+    res.status(200).send("Testing...");
+});
 router.post('/one', (req, res) => {
-    boleto({
-        clientId: process.env.ID,
-        clientSecret: process.env.SECRET
-    }, {
-        // options banking billet
-    });
-    res.status(201).json(req.body);
+    const options = {
+            client_id: process.env.ID,
+            client_secret: process.env.SECRET,
+            sandbox: true
+        },
+        dueDate = moment().add(3, 'days').format('YYYY-MM-DD'),
+        paymentBody = {
+            payment: {
+                banking_billet: {
+                    expire_at: dueDate,
+                    customer: {
+                        name: req.body.customer.name,
+                        cpf: req.body.customer.cpf,
+                        phone_number: req.body.customer.phone
+                    },
+                    configurations: {
+                        fine: 0,
+                        interest: 33
+                    }
+                }
+            },
+            items: [{
+                name: req.body.products.name,
+                value: +req.body.products.value,
+                amount: +req.body.products.amount
+            }],
+            shippings: [{
+                name: 'Taxa para processar o pagamento.',
+                value: 299
+            }]
+        },
+        gn = new Gerencianet(options);
+
+
+        gn.oneStep({}, paymentBody)
+            .then((data) => {
+                console.log(data);
+                res.status(200).json(data);
+            }).catch((err) => {
+                console.log(`
+                ########### ERROR ###########
+                ${err}
+                `);
+                res.status(501).json(err);
+            });
+
+    //res.status(201).json(link);
     
 });
 
